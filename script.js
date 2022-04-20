29711d1b134b481a0dd48a3932016d6a0059936d
Dmitry Gorbach <89874530087@mail.ru>
2022-04-20 21:52:31 +0200

    add lesson16

--- script.js
+++ script.js
@@ -1,41 +1,21 @@
-function DomElement() {
-  this.selector = prompt("имя класса или id");
-  this.height = prompt("высота", 100);
-  this.width = prompt("ширина", 100);
-  this.bg = prompt("фон", "red");
-  this.fontSize = prompt("размер шрифта", 26);
-
-  this.createElem = function () {
-    let text = prompt("любой текст", "text");
-
-    const div = document.createElement("div");
-
-    document.body.append(div);
-
-    if (this.selector.trim().split("")[0] === ".") {
-      div.classList.add(this.selector.trim().substr(1));
-    }
-
-    if (this.selector.trim().split("")[0] === "#") {
-      div.id = this.selector.trim().substr(1);
-    }
-
-    div.style.height = this.height + "px";
-    div.style.width = this.width + "px";
-    div.style.background = this.bg;
-    div.style.fontSize = this.fontSize + "px";
-    div.textContent = text;
-  };
+class First {
+  constructor() {}
+  hello() {
+    console.log("Привет, я метод родителя");
+  }
 }
 
-const elem = new DomElement();
+const newFirst = new First();
 
-elem.createElem();
+class Second extends First {
+  constructor() {
+    super();
+  }
+  hello() {
+    newFirst.hello();
+    console.log("А я наследуемый метод!");
+  }
+}
 
-/* console.log(elem);
-    console.log("новый div", div);
-    console.log("textContent", div.textContent);
-    console.log("height", div.style.height);
-    console.log("width", div.style.width);
-    console.log("bg", div.style.background);
-    console.log("fontSize", div.style.fontSize); */
+const newSecond = new Second();
+newSecond.hello();
